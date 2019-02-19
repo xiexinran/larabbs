29f4ee9aef572fe2b94f44a00a853c34b5c23a54
xiexinran <781473919@qq.com>
2019-02-19 22:09:53 +0800

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            好好学习，天天向上<span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+    </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title','首页')
+
+@section('content')
+    <h1>今天一点也没有成就感，感觉好难做</h1>
+@stop
