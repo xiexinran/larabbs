69ee16fb9cf1edadde456e732359d2d555998755
xiexinran <781473919@qq.com>
2019-04-09 22:34:26 +0800

    Generated topics

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,10 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
+	{
+		\App\Models\User::observe(\App\Observers\UserObserver::class);
+		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+
         //carbon是datetime操作扩展，laravel将其默认集成到了框架中，使用setlocale设置成中文提示
         \Carbon\Carbon::setLocale('zh');
     }
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 //UsersController@show中的show方法传参时申明了类型User,show方法中对应的参数&user会匹配路由片段中的{user}，{user}对应请求路由中的用户ID
 //当请求 http://larabbs.test/users/1 并且满足以上两个条件时，Laravel 将会自动查找 ID 为 1 的用户并赋值到变量UsersController@show中的 $user 中
 Route::get('/users/{user}','UsersController@show')->name('users.show');
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
